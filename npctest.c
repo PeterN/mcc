081964d68a75228f9a109f9f22039ce4cb982aed
Peter Nelson <petern@lachesis.(none)>
2011-01-11 13:17:23 +0000

    Restructure npctest, including player following.

--- npctest.c
+++ npctest.c
@@ -3,17 +3,15 @@
 #include "client.h"
 #include "player.h"
 #include "level.h"
+#include "astar.h"
 
 #define NPC 64
 
-struct npctesto
+struct npctemp
 {
-	int i;
-	struct {
-		char name[64];
-		struct position_t pos;
-		struct npc *npc;
-	} n[32];
+	struct npc *npc[NPC];
+	struct point *path[NPC];
+	int step[NPC];
 };
 
 struct npctest
@@ -22,12 +20,10 @@ struct npctest
 	struct {
 		char name[64];
 		struct position_t pos;
-		struct npc *npc;
-	} n[NPC];
-	struct {
 		int8_t followid;
 		int8_t stareid;
-	} f[NPC];
+	} n[NPC];
+	struct npctemp *temp;
 };
 
 void npctest_save(struct level_t *l, struct npctest *arg)
@@ -35,34 +31,30 @@ void npctest_save(struct level_t *l, struct npctest *arg)
 	int i;
 	for (i = 0; i < NPC; i++)
 	{
-		if (arg->n[i].npc != NULL)
+		if (arg->temp->npc[i] != NULL)
 		{
-			arg->n[i].pos = arg->n[i].npc->pos;
+			arg->n[i].pos = arg->temp->npc[i]->pos;
 		}
 	}
 }
 
 void npctest_init(struct level_t *l, struct npctest *arg)
 {
+	arg->temp = malloc(sizeof *arg->temp);
+	memset(arg->temp, 0, sizeof *arg->temp);
+
 	int i;
 	for (i = 0; i < NPC; i++)
 	{
 		if (arg->n[i].name[0] != '\0')
 		{
-			arg->n[i].npc = npc_add(l, arg->n[i].name, arg->n[i].pos);
+			arg->temp->npc[i] = npc_add(l, arg->n[i].name, arg->n[i].pos);
 		}
 		else
 		{
-			arg->n[i].npc = NULL;
+			arg->temp->npc[i] = NULL;
 		}
 	}
-}
-
-void npctest_init_old(struct level_t *l, struct npctest *arg)
-{
-	memset(arg->f, 0, sizeof arg->f);
-
-	npctest_init(l, arg);
 }
 
 void npctest_deinit(struct level_t *l, struct npctest *arg)
@@ -72,11 +64,15 @@ void npctest_deinit(struct level_t *l, struct npctest *arg)
 	int i;
 	for (i = 0; i < NPC; i++)
 	{
-		if (arg->n[i].npc != NULL)
+		if (arg->temp->npc[i] != NULL)
 		{
-			npc_del(arg->n[i].npc);
+			npc_del(arg->temp->npc[i]);
 		}
+
+		free(arg->temp->path[i]);
 	}
+
+	free(arg->temp);
 }
 
 static int npctest_get_by_name(struct npctest *arg, const char *name)
@@ -107,13 +103,13 @@ static int npctest_get_by_name(struct npctest *arg, const char *name)
 	return -1;
 }
 
-static void npctest_find_nearest(struct level_t *l, int i, struct npctest *arg)
+static void npctest_find_nearest(struct level_t *l, int i, struct npctest *arg, int8_t *out)
 {
 	long mindist = INT_MAX;
 	int nearest = -1;
 	int j;
 
-	const struct position_t us = arg->n[i].npc->pos;
+	const struct position_t us = arg->temp->npc[i]->pos;
 
 	for (j = 0; j < MAX_CLIENTS_PER_LEVEL; j++)
 	{
@@ -134,8 +130,47 @@ static void npctest_find_nearest(struct level_t *l, int i, struct npctest *arg)
 
 	if (nearest != -1)
 	{
-		arg->f[i].stareid = nearest + 1;
+		*out = nearest + 1;
 	}
+}
+
+static void npctest_replacepath(struct level_t *l, struct npctest *arg, int i)
+{
+	free(arg->temp->path[i]);
+	arg->temp->path[i] = NULL;
+
+	int nearest = arg->n[i].followid - 1;
+	if (l->clients[nearest] == NULL) return;
+
+	const struct position_t us = arg->temp->npc[i]->pos;
+	const struct position_t them = l->clients[nearest]->player->pos;
+
+	struct point a;
+	struct point b;
+
+	a.x = us.x / 32;
+	a.y = us.z / 32;
+	a.z = us.y / 32;
+
+	b.x = them.x / 32;
+	b.y = them.z / 32;
+	b.z = them.y / 32;
+
+	for (; a.z > 0 && level_get_blocktype(l, a.x, a.z, a.y) == AIR; a.z--);
+	for (; b.z > 0 && level_get_blocktype(l, b.x, b.z, b.y) == AIR; b.z--);
+
+	a.z++;
+	b.z++;
+
+	printf("Pathfinding from %d %d %d to %d %d %d\n", a.x, a.y, a.z, b.x, b.y, b.z);
+
+	arg->temp->path[i] = as_find(l, &a, &b);
+	arg->temp->step[i] = 0;
+}
+
+static inline int min(int a, int b)
+{
+	return a < b ? a : b;
 }
 
 static void npctest_handle_tick(struct level_t *l, struct npctest *arg)
@@ -143,21 +178,77 @@ static void npctest_handle_tick(struct level_t *l, struct npctest *arg)
 	arg->i++;
 
 	int i = arg->i % 64;
-	if (arg->f[i].stareid != 0) npctest_find_nearest(l, i, arg);
+	if (arg->n[i].stareid != 0) npctest_find_nearest(l, i, arg, &arg->n[i].stareid);
+	if (arg->n[i].followid != 0)
+	{
+		int8_t followid = arg->n[i].followid;
+		npctest_find_nearest(l, i, arg, &arg->n[i].followid);
+		if (followid != arg->n[i].followid)
+		{
+			npctest_replacepath(l, arg, i);
+		}
+		arg->n[i].stareid = arg->n[i].followid;
+	}
+
+	for (i = 0; i < NPC; i++)
+	{
+		struct position_t *us = &arg->temp->npc[i]->pos;
+
+		if (arg->n[i].followid == 0) continue;
+		if (arg->temp->path[i] == NULL)
+		{
+			npctest_replacepath(l, arg, i);
+		}
+		else
+		{
+			const struct point *p = arg->temp->path[i] + arg->temp->step[i];
+			if (p->x == -1)
+			{
+				npctest_replacepath(l, arg, i);
+			}
+			else
+			{
+				int dx = (p->x * 32 + 16) - us->x;
+				int dy = (p->z * 32 + 48) - us->y;
+				int dz = (p->y * 32 + 16) - us->z;
+
+				int sp = 5;
+
+				if (dx < 0) us->x -= min(abs(dx), sp);
+				else if (dx > 0) us->x += min(dx, sp);
+				if (dy < 0) us->y -= min(abs(dy), sp);
+				else if (dy > 0) us->y += min(dy, sp);
+				if (dz < 0) us->z -= min(abs(dz), sp);
+				else if (dz > 0) us->z += min(dz, sp);
+
+				int bx = us->x / 32;
+				int by = us->y / 32 - 1;
+				int bz = us->z / 32;
+
+				if (p->x == bx && p->z == by && p->y == bz)
+				{
+					arg->temp->step[i]++;
+					printf("Moved to step %d\n", arg->temp->step[i]);
+				}
+			}
+		}
+	}
 
 	const int step = 8;
 	for (i = arg->i % step; i < NPC; i += step)
 	{
-		if (arg->f[i].stareid == 0) continue;
+		struct position_t *us = &arg->temp->npc[i]->pos;
 
-		int nearest = arg->f[i].stareid - 1;
+		if (arg->n[i].stareid == 0) continue;
+
+		int nearest = arg->n[i].stareid - 1;
 		if (l->clients[nearest] == NULL) continue;
 
-		const struct position_t us = arg->n[i].npc->pos;
-		const struct position_t them = l->clients[nearest]->player->pos;
-		int dx = us.x - them.x;
-		int dy = us.y - them.y;
-		int dz = us.z - them.z;
+		//const struct position_t us = arg->temp->npc[i]->pos;
+		const struct position_t *them = &l->clients[nearest]->player->pos;
+		int dx = us->x - them->x;
+		int dy = us->y - them->y;
+		int dz = us->z - them->z;
 
 		//float r = sqrtf(dx * dx + dy * dy + dz * dz);
 		float azimuth = atan2(dz, dx);
@@ -167,8 +258,8 @@ static void npctest_handle_tick(struct level_t *l, struct npctest *arg)
 		//	arg->n[i].name, l->clients[nearest]->player->username,
 		//	r, azimuth * 360 / (2 * M_PI), zenith * 360 / (2 * M_PI));
 
-		arg->n[i].npc->pos.h = azimuth * 256 / (2 * M_PI) - 64;
-		arg->n[i].npc->pos.p = zenith * 256 / (2 * M_PI);
+		us->h = azimuth * 256 / (2 * M_PI) - 64;
+		us->p = zenith * 256 / (2 * M_PI);
 	}
 }
 
@@ -186,9 +277,9 @@ static bool npctest_handle_chat(struct level_t *l, struct client_t *c, char *dat
 			if (arg->n[i].name[0] == '\0')
 			{
 				snprintf(arg->n[i].name, sizeof arg->n[i].name, data + 8);
-				arg->n[i].npc = npc_add(l, arg->n[i].name, c->player->pos);
-				arg->f[i].followid = 0;
-				arg->f[i].stareid = 0;
+				arg->temp->npc[i] = npc_add(l, arg->n[i].name, c->player->pos);
+				arg->n[i].followid = 0;
+				arg->n[i].stareid = 0;
 
 				snprintf(buf, sizeof buf, TAG_YELLOW "%s created", data + 8);
 				break;
@@ -204,9 +295,11 @@ static bool npctest_handle_chat(struct level_t *l, struct client_t *c, char *dat
 		{
 			snprintf(buf, sizeof buf, TAG_YELLOW "%s deleted", arg->n[i].name);
 
-			npc_del(arg->n[i].npc);
+			npc_del(arg->temp->npc[i]);
+			free(arg->temp->path[i]);
 			arg->n[i].name[0] = '\0';
-			arg->n[i].npc = NULL;
+			arg->temp->npc[i] = NULL;
+			arg->temp->path[i] = NULL;
 		}
 		else
 		{
@@ -219,7 +312,7 @@ static bool npctest_handle_chat(struct level_t *l, struct client_t *c, char *dat
 
 		if (i >= 0)
 		{
-			arg->n[i].npc->pos = c->player->pos;
+			arg->temp->npc[i]->pos = c->player->pos;
 			snprintf(buf, sizeof buf, TAG_YELLOW "Summoned %s", arg->n[i].name);
 		}
 		else
@@ -232,12 +325,26 @@ static bool npctest_handle_chat(struct level_t *l, struct client_t *c, char *dat
 		int i = npctest_get_by_name(arg, data + 7);
 		if (i >= 0)
 		{
-			player_teleport(c->player, &arg->n[i].npc->pos, true);
+			player_teleport(c->player, &arg->temp->npc[i]->pos, true);
 			return true;
 		}
 		else
 		{
 			snprintf(buf, sizeof buf, TAG_YELLOW "%s not found", data + 7);
+		}
+	}
+	else if (strncasecmp(data, "npc follow ", 11) == 0)
+	{
+		int i = npctest_get_by_name(arg, data + 11);
+
+		if (i >= 0)
+		{
+			arg->n[i].followid = !arg->n[i].followid;
+			snprintf(buf, sizeof buf, TAG_YELLOW "%s now %sfollowing", arg->n[i].name, arg->n[i].followid ? "" : "not ");
+		}
+		else
+		{
+			snprintf(buf, sizeof buf, TAG_YELLOW "%s not found", data + 10);
 		}
 	}
 	else if (strncasecmp(data, "npc stare ", 10) == 0)
@@ -246,8 +353,8 @@ static bool npctest_handle_chat(struct level_t *l, struct client_t *c, char *dat
 
 		if (i >= 0)
 		{
-			arg->f[i].stareid = !arg->f[i].stareid;
-			snprintf(buf, sizeof buf, TAG_YELLOW "%s now %sstaring", arg->n[i].name, arg->f[i].stareid ? "" : "not ");
+			arg->n[i].stareid = !arg->n[i].stareid;
+			snprintf(buf, sizeof buf, TAG_YELLOW "%s now %sstaring", arg->n[i].name, arg->n[i].stareid ? "" : "not ");
 		}
 		else
 		{
@@ -261,11 +368,15 @@ static bool npctest_handle_chat(struct level_t *l, struct client_t *c, char *dat
 		{
 			if (arg->n[i].name[0] != '\0')
 			{
-				npc_del(arg->n[i].npc);
+				npc_del(arg->temp->npc[i]);
+				free(arg->temp->path[i]);
 				arg->n[i].name[0] = '\0';
+				arg->n[i].followid = 0;
+				arg->n[i].stareid = 0;
+				arg->temp->npc[i] = NULL;
+				arg->temp->path[i] = NULL;
 			}
 		}
-		memset(arg, 0, sizeof (struct npctest));
 		snprintf(buf, sizeof buf, TAG_YELLOW "NPCs wiped");
 	}
 	else if (strcasecmp(data, "npc list") == 0)
@@ -333,13 +444,6 @@ static bool npctest_level_hook(int event, struct level_t *l, struct client_t *c,
 					npctest_init(l, arg->data);
 					break;
 				}
-				else if (arg->size == sizeof (struct npctesto))
-				{
-					arg->size = sizeof (struct npctest);
-					arg->data = realloc(arg->data, arg->size);
-					npctest_init_old(l, arg->data);
-					break;
-				}
 
 				LOG("Found invalid npctest data on %s, erasing\n", l->name);
 				free(arg->data);
@@ -347,6 +451,10 @@ static bool npctest_level_hook(int event, struct level_t *l, struct client_t *c,
 
 			arg->size = sizeof (struct npctest);
 			arg->data = calloc(1, arg->size);
+
+			struct npctest *npc = arg->data;
+			npc->temp = malloc(sizeof *npc->temp);
+			memset(npc->temp, 0, sizeof *npc->temp);
 			break;
 		}
 		case EVENT_DEINIT:
