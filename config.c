cd98563dcd356693f27e45555a685ff91cd74109
Peter Nelson <peter@fuzzle.org>
2011-01-15 20:51:54 +0000

    Missing include and declaration mismatch.

--- config.c
+++ config.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "config.h"
 #include "mcc.h"
 
 #define MAXCONFIGLEN 64
--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 void config_init(const char *filename);
 void config_deinit(void);
 
-void config_set_string(const char *setting, char *value);
+void config_set_string(const char *setting, const char *value);
 void config_set_int(const char *setting, int value);
 
 bool config_get_string(const char *setting, char **value);
