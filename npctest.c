61569e112fb2690f626799cea236028ede2853e9
Peter Nelson <peter@fuzzle.org>
2011-01-21 07:45:28 +0000

    Fix stupid bug that prevented a new path being used.

--- npctest.c
+++ npctest.c
@@ -33,7 +33,6 @@ struct npctemp
 	float hs, vs;       /* Horizontal / vertical speed */
 	bool iw;            /* In-water? */
 	bool pf;            /* Pathfinding? */
-	bool debug;         /* Debug? */
 };
 
 struct npcinfo
@@ -240,27 +239,13 @@ static void npc_astar_cb(struct level_t *l, struct point *path, void *data)
 	struct npcdata *nd = arg->data;
 	struct npctemp *ni = &nd->t[temp->i];
 
-	if (ni->path != NULL)
+	if (path != NULL)
 	{
-		if (ni->debug)
-		{
-			snprintf(buf, sizeof buf, "%s: Got a path", nd->n[temp->i].name);
-			level_notify_all(l, buf);
-		}
-
 		void *oldpath = ni->path;
 		ni->path = path;
 		ni->step = 0;
 		nd->n[temp->i].stareid = 0;
 		free(oldpath);
-	}
-	else
-	{
-		if (ni->debug)
-		{
-			snprintf(buf, sizeof buf, "%s: Couldn't get a path", nd->n[temp->i].name);
-			level_notify_all(l, buf);
-		}
 	}
 
 	ni->pf = false;
@@ -370,18 +355,8 @@ static void npc_replacepath(struct level_t *l, int i, struct level_hook_data_t *
 	float bz = them->z / 32.0f;
 	float by = them->y / 32.0f - HEIGHT;
 
-	if (ni->debug)
-	{
-		snprintf(buf, sizeof buf, "%s: Finding path from %f %f %f to %f %f %f", nd->n[i].name, ax, ay, az, bx, by, bz);
-		level_notify_all(l, buf);
-	}
-
+	for (; by < l->y - 1 && !npc_4point(l, bx, by, bz); by++);
 	for (; by > 0 && npc_4point(l, bx, by - 1, bz); by--);
-	if (ni->debug && by != them->y / 32.0f - HEIGHT)
-	{
-		snprintf(buf, sizeof buf, "%s: Adjusted target to %f", nd->n[i].name, by);
-		level_notify_all(l, buf);
-	}
 
 	struct point a;
 	a.x = ax; a.y = az; a.z = ay;
@@ -401,12 +376,6 @@ static void npc_replacepath(struct level_t *l, int i, struct level_hook_data_t *
 	if (npc_iswalkable(l, ax, ay, az, bx, by, bz))
 	{
 		/* We can walk directly to our tndet, so don't bother pathfinding */
-		if (ni->debug)
-		{
-			snprintf(buf, sizeof buf, "%s: Can walk directly", nd->n[i].name);
-			level_notify_all(l, buf);
-		}
-
 		free(ni->path);
 		ni->path = malloc(3 * sizeof (struct point));
 		ni->step = 0;
@@ -414,12 +383,6 @@ static void npc_replacepath(struct level_t *l, int i, struct level_hook_data_t *
 		ni->path[1] = b;
 		ni->path[2].x = -1;
 		return;
-	}
-
-	if (ni->debug)
-	{
-		snprintf(buf, sizeof buf, "%s: Waiting for a path, final search: %d %d %d to %d %d %d", nd->n[i].name, a.x, a.z, a.y, b.x, b.z, b.y);
-		level_notify_all(l, buf);
 	}
 
 	struct pftemp *temp = malloc(sizeof *temp);
@@ -702,19 +665,6 @@ static bool npc_handle_chat(struct level_t *l, struct client_t *c, char *data, s
 		else
 		{
 			snprintf(buf, sizeof buf, TAG_YELLOW "%s not found", data + 7);
-		}
-	}
-	else if (strncasecmp(data, "npc debug ", 10) == 0)
-	{
-		int i = npc_get_by_name(nd, data + 10);
-		if (i >= 0)
-		{
-			nd->t[i].debug = !nd->t[i].debug;
-			snprintf(buf, sizeof buf, TAG_YELLOW "Debug %sabled for %s", nd->t[i].debug ? "en" : "dis", nd->n[i].name);
-		}
-		else
-		{
-			snprintf(buf, sizeof buf, TAG_YELLOW "%s not found", data + 10);
 		}
 	}
 	else if (strncasecmp(data, "npc follow ", 11) == 0)
